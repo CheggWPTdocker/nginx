82efaa0fb6e828ad7fec45da2db4243279e4ba2f
Joel Gilley <gilleyj@gmail.com>
2017-01-18 16:58:51 -0500

    Try the right ports maybe?

--- .travis/test_gzip.sh
+++ .travis/test_gzip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo -n 'GZIP test: '
-curl -vsf -H 'Accept-Encoding: gzip' 'http://localhost:8080/' &> /dev/stdout | grep -i 'Content-Encoding: gzip'
+curl -vsf -H 'Accept-Encoding: gzip' 'http://localhost:80/' &> /dev/stdout | grep -i 'Content-Encoding: gzip'
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
     echo OK
--- .travis/test_http.sh
+++ .travis/test_http.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo -n 'HTTP test: '
-curl -vsf -H 'Accept-Encoding: gzip' 'http://localhost:8080/' &> /dev/stdout | grep -i '200 OK'
+curl -vsf -H 'Accept-Encoding: gzip' 'http://localhost:80/' &> /dev/stdout | grep -i '200 OK'
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
     echo OK
